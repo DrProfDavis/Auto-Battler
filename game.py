b97cd208ef66d47275b10e634dcecd3830d4cd2f
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-11-29 18:41:16 -0600

    Player and image

--- game.py
+++ game.py
@@ -5,17 +5,30 @@ import sys
 pygame.init()
 
 # Set up display
-width, height = 800, 600
+width, height = 720/2, 1280/2
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Simple Pygame App")
+pygame.display.set_caption("Pygame App")
 
-# Set up colors
-red = (255, 0, 0)
+# Set up the background image
+background_image = pygame.image.load("./Projects/Auto-Battler/images/background.png")
+original_bg_width, original_bg_height = background_image.get_size()
+scaled_bg_width = original_bg_width // 3
+scaled_bg_height = original_bg_height // 3
+background_image = pygame.transform.scale(background_image, (scaled_bg_width, scaled_bg_height))
 
-# Set up the player rectangle
-player_size = 50
-player_x = (width - player_size) // 2
-player_y = height - player_size - 10
+
+# Set up the player image
+player_image = pygame.image.load("./Projects/Auto-Battler/images/cowboy.png")
+original_width, original_height = player_image.get_size()
+scaled_width = 180  # Adjust this to your desired width
+scaled_height = int((scaled_width / original_width) * original_height)
+player_image = pygame.transform.scale(player_image, (scaled_width, scaled_height))
+player_rect = player_image.get_rect()
+
+# Set the player's initial position higher and to the right on the screen
+initial_x_position = (width - scaled_width) // 2 + -100  # Adjust this to your desired x-coordinate
+initial_y_position = height - scaled_height - 350  # Adjust this to your desired y-coordinate
+player_rect.topleft = (initial_x_position, initial_y_position)
 
 # Set up clock to control the frame rate
 clock = pygame.time.Clock()
@@ -31,16 +44,19 @@ while True:
     keys = pygame.key.get_pressed()
 
     # Update player position based on arrow key input
-    if keys[pygame.K_LEFT] and player_x > 0:
-        player_x -= 5
-    if keys[pygame.K_RIGHT] and player_x < width - player_size:
-        player_x += 5
+    if keys[pygame.K_LEFT] and player_rect.left > 0:
+        player_rect.x -= 5
+    if keys[pygame.K_RIGHT] and player_rect.right < width:
+        player_rect.x += 5
 
     # Clear the screen
     screen.fill((255, 255, 255))
 
-    # Draw the player rectangle
-    pygame.draw.rect(screen, red, (player_x, player_y, player_size, player_size))
+    # Draw the background image
+    screen.blit(background_image, (0, -65))
+
+    # Draw the scaled player image
+    screen.blit(player_image, player_rect)
 
     # Update the display
     pygame.display.flip()
